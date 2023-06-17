d2300fe402b61efd7a29073fbe0dc37f62a6a45b
Ricard <231197@virtualwindow.co.za>
2023-06-17 10:40:27 +0200

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -3,10 +3,11 @@ console.log(finaltotal);
 var total = document.getElementById("finaltotal");
 total.textContent = finaltotal;
 
+//let confirmedCodes = ["FH33", "GGH22", "DOL"];
 let confirmedCodes = [
-  { discount: 10, code: "FFHH33" },
+  { discount: 10, code: "FHH33" },
   { discount: 25, code: "GGH22" },
-  { discount: 50, code: "DALOL" },
+  { discount: 50, code: "DOL" },
 ];
 const discount = () => {
   let inputValue = document.getElementById("val").value;
